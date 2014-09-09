79a88afad3cb0dccc454de45d5b1156fdab8027f
Jessica Goulding <jgoulding2@gmail.com>
2014-09-09 14:38:48 -0600

    added ranking

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,10 +1,24 @@
 class Idea
   attr_reader :title,
-              :description
+              :description,
+              :rank
 
   def initialize(attributes)
     @title       = attributes["title"]
     @description = attributes["description"]
+    @rank        = attributes["rank"] || 0
+  end
+
+  def save
+    IdeaStore.create(to_h)
+  end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => rank
+    }
   end
 
 end
