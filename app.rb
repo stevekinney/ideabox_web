0fe38d2a9164cdfecdd38fb22fc8066a2e1908b3
Jessica Goulding <jgoulding2@gmail.com>
2014-09-09 14:23:21 -0600

    refactored Idea, created idea_store and moved methods to be more consistent

--- app.rb
+++ app.rb
@@ -1,12 +1,11 @@
-require 'bundler'
 require './idea'
-Bundler.require
+require './idea_store'
 
 class IdeaBoxApp < Sinatra::Base
   set :method_override, true
 
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
   end
 
   not_found do
@@ -14,23 +13,22 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   post '/' do
-    idea = Idea.new(params[:idea])
-    idea.save
+    IdeaStore.create(params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete(id.to_i)
+    IdeaStore.delete(id.to_i)
     redirect '/'
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find(id.to_i)
+    idea = IdeaStore.find(id.to_i)
     erb :edit, locals: {id: id, idea: idea}
   end
 
   put '/:id' do |id|
-    Idea.update(id.to_i, params[:idea])
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 
--- idea.rb
+++ idea.rb
@@ -1,5 +1,3 @@
-require 'yaml/store'
-
 class Idea
   attr_reader :title,
               :description
@@ -7,55 +5,6 @@ class Idea
   def initialize(attributes)
     @title       = attributes["title"]
     @description = attributes["description"]
-  end
-
-  def self.database
-    @database ||= YAML::Store.new('ideabox')
-  end
-
-  def self.all
-    raw_ideas.map do |data|
-      Idea.new(data)
-    end
-  end
-
-  def self.raw_ideas
-    database.transaction do |db|
-      db['ideas'] || []
-    end
-  end
-
-  def self.delete(position)
-    database.transaction do
-      database['ideas'].delete_at(position)
-    end
-  end
-
-  def self.find(id)
-    Idea.new(find_raw_idea(id))
-  end
-
-  def self.find_raw_idea(id)
-    database.transaction do
-      database['ideas'].at(id)
-    end
-  end
-
-  def self.update(id, data)
-    database.transaction do
-      database['ideas'][id] = data
-    end
-  end
-
-  def database
-    Idea.database
-  end
-
-  def save
-    database.transaction do |db|
-      db['ideas'] ||= []
-      db['ideas'] << {"title" => title, "description" => description}
-    end
   end
 
 end
